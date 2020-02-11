fde952786258d9ba785e6f169457526b1a931b50
nixberg <k.nixberg@gmail.com>
2020-02-11 17:09:56 +0100

    Add AES test

--- Tests/AES128Tests/AES128Tests.swift
+++ Tests/AES128Tests/AES128Tests.swift
@@ -4,6 +4,25 @@ import CipherModes
 
 final class AES128Tests: XCTestCase {
     func testAES128() {
+        let aes = AES128(key: [
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
+        ])
+        
+        let plaintext: [UInt8] = [
+            0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+            0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff
+        ]
+        let ciphertext: [UInt8] = [
+            0x69, 0xc4, 0xe0, 0xd8, 0x6a, 0x7b, 0x04, 0x30,
+            0xd8, 0xcd, 0xb7, 0x80, 0x70, 0xb4, 0xc5, 0x5a
+        ]
+        
+        XCTAssertEqual(aes.encryptECBWithoutPadding(plaintext), ciphertext)
+        XCTAssertEqual(aes.decryptECBWithoutPadding(ciphertext), plaintext)
+    }
+    
+    func testAES128RoundTrip() {
         let aes = AES128(key: [UInt8].random(count: AES128.blockSize))
         let input = [UInt8].random(count: AES128.blockSize)
         
@@ -15,7 +34,7 @@ final class AES128Tests: XCTestCase {
         XCTAssertEqual(input, decrypted)
     }
     
-    func testECB() {
+    func testECBRoundTrip() {
         let aes = AES128(key: [UInt8].random(count: AES128.blockSize))
         let input = [UInt8].random(count: .random(in: 0..<1024))
         
@@ -24,7 +43,7 @@ final class AES128Tests: XCTestCase {
         XCTAssertEqual(input, decrypted)
     }
     
-    func testCBC() {
+    func testCBCRoundTrip() {
         let aes = AES128(key: [UInt8].random(count: AES128.blockSize))
         let input = [UInt8].random(count: .random(in: 0..<1024))
         let iv = [UInt8].random(count: AES128.blockSize)
@@ -34,7 +53,7 @@ final class AES128Tests: XCTestCase {
         XCTAssertEqual(input, decrypted)
     }
     
-    func testCTR() {
+    func testCTRRoundTrip() {
         let aes = AES128(key: [UInt8].random(count: AES128.blockSize))
         let input = [UInt8].random(count: .random(in: 0..<1024))
         let nonce = UInt64.random(in: 0..<UInt64.max)
